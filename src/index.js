b28211c09f7068b031bb7d560213917362d03f49
katerina2018 <tvorchestvo15@gmail.com>
2021-12-14 15:00:43 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './sass/main.scss';
+
+import Notiflix from 'notiflix';
+
+import cardTpl from './templates/card.hbs';
+
+import ImgApiService from './js/img-service';
+import LoadMoreBtn from './js/components/load-more-btn';
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    searchValue: document.querySelector('[name="searchQuery"]'),
+    searchBtn: document.querySelector('#search-form button '),
+    galleryContainer: document.querySelector('.gallery'),
+};
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
+const imgApiService = new ImgApiService();
+
+refs.form.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchImgs);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    imgApiService.query = e.currentTarget.elements[0].value;
+
+    if (imgApiService.query === '') {
+        Notiflix.Notify.warning('Ведите запрос');
+        return;
+    }
+
+    imgApiService.resetPage();
+    clearItemsContainer();
+    fetchImgs();
+
+    loadMoreBtn.show();
+}
+
+function fetchImgs() {
+    loadMoreBtn.disable();
+    imgApiService.fetchImgs().then(items => {
+        appendImgsMarkup(items);
+        loadMoreBtn.enable();
+    });
+}
+
+function appendImgsMarkup(items) {
+    refs.galleryContainer.insertAdjacentHTML('beforeend', cardTpl(items));
+}
+
+function clearItemsContainer() {
+    refs.galleryContainer.innerHTML = '';
+}
---
+++ src/js/img-service.js
@@ -0,0 +1,45 @@
+import Notiflix from 'notiflix';
+
+const API_KEY = '24782387-235d5961f89ca8adc0055c0c3';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class ImgApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchImgs() {
+        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&language=en&per_page=40&page=${this.page}`;
+
+        return fetch(url)
+            .then(response => response.json())
+            .then(({ hits }) => {
+                if (hits.length === 0) {
+                    Notiflix.Notify.warning(
+                        '"Sorry, there are no images matching your search query. Please try again.',
+                    );
+                    return;
+                }
+                this.incrementPage();
+
+                return hits;
+            });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
